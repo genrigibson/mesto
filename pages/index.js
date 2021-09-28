5375d6e0cedfe40b8b3904aa452295de29960675
Denis Khnykin <toshirkooo@gmail.com>
2021-09-28 16:34:48 +0300

    js version 1

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,23 @@
-const popUpContainer = document.querySelector('.popup__container');
-const editInfo = document.querySelector('#edit-info');
-function popUpOpen() {
-  popUpContainer.classList.toggle('.popup__container');
-  popUpContainer.classList.toggle('.popup_opened');
+const popUp = document.querySelector('.popup');
+const popUpCloseBtn = document.querySelector('.popup__close');
+const popUpOpenBtn = document.querySelector('.profile__info-button');
+let formElement = document.querySelector('#profileForm');
+
+function togglePopUp() {
+  popUp.classList.toggle('popup__opened');
 }
-editInfo.addEventListener('click', popUpOpen());
+popUpOpenBtn.addEventListener('click', togglePopUp);
+popUpCloseBtn.addEventListener('click', togglePopUp);
 
-```jsx
-// Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-												// Так мы можем определить свою логику отправки.
-												// О том, как это делать, расскажем позже.
+	evt.preventDefault();
+	let nameInput = document.querySelector('#nameInput');
+	let jobInput = document.querySelector('#jobInput');
+  let nameInfo = document.querySelector('#nameInfo');
+  let jobInfo = document.querySelector('#jobInfo');
 
-	// Находим поля формы в DOM
-	let nameInput = // Воспользуйтесь инструментом .querySelector()
-	let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-	// Получите значение полей из свойства value
-
-	// Выберите элементы, куда должны быть вставлены значения полей
-
-	// Вставьте новые значения с помощью textContent
+  nameInfo.textContent = nameInput.value;
+  jobInfo.textContent = jobInput.value;
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-```
+
