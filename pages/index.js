95f7f31309d252de1c7f9bd55dc97bab055fa077
Denis Khnykin <toshirkooo@gmail.com>
2021-10-01 12:10:14 +0300

    version 1.2

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,24 @@ const popUp = document.querySelector('.popup');
 const popUpCloseBtn = document.querySelector('.popup__close');
 const popUpOpenBtn = document.querySelector('.profile__info-button');
 let formElement = document.querySelector('#profileForm');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
+let nameInfo = document.querySelector('#nameInfo');
+let jobInfo = document.querySelector('#jobInfo');
 
 function togglePopUp() {
-  popUp.classList.toggle('popup__opened');
+  popUp.classList.toggle('popup_opened');
+  nameInput.value = nameInfo.textContent;
+  jobInput.value = jobInfo.textContent;
 }
 popUpOpenBtn.addEventListener('click', togglePopUp);
 popUpCloseBtn.addEventListener('click', togglePopUp);
 
 function formSubmitHandler (evt) {
 	evt.preventDefault();
-	let nameInput = document.querySelector('#nameInput');
-	let jobInput = document.querySelector('#jobInput');
-  let nameInfo = document.querySelector('#nameInfo');
-  let jobInfo = document.querySelector('#jobInfo');
-
   nameInfo.textContent = nameInput.value;
   jobInfo.textContent = jobInput.value;
+  togglePopUp();
 }
 formElement.addEventListener('submit', formSubmitHandler);
 
