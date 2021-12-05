0b37b1f9ce8791c5c9d6652275d58d642e80d5a7
Denis Khnykin <toshirkooo@gmail.com>
2021-12-05 20:15:02 +0300

    7 sprint

--- pages/FormValidator.js
+++ pages/FormValidator.js
@@ -2,7 +2,7 @@
 
 export default class FormValidator {
   constructor(settings, formSelector) {
-      this._formSelector = formSelector,
+    this._formSelector = formSelector,
       this._inputSelector = settings.inputSelector,
       this._submitButtonSelector = settings.submitButtonSelector,
       this._inactiveButtonClass = settings.inactiveButtonClass,
@@ -11,7 +11,7 @@ export default class FormValidator {
   }
 
 
-  enableValidation = () => {
+  enableValidation() {
     this._setEventListeners();
   }
 
@@ -30,9 +30,10 @@ export default class FormValidator {
     })
 
     this._toggleButtonState();
-
+    this._validateLength();
+    this._validateRequired();
+    this._validateLink();
   }
-
 
 
   _checkInputValidity(input) {
@@ -65,94 +66,25 @@ export default class FormValidator {
     button.disabled = !this._formSelector.checkValidity();
     button.classList.toggle(this._inactiveButtonClass, !this._formSelector.checkValidity());
   }
-}
 
-/*export const validationConfig = {
-  formSelector: ".popup__container-item",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: ".popup__button_invalid",
-  inputErrorClass: ".error",
-  errorClass: ".popup__input_state_invalid",
-};
-function enableValidation(config) {
-  const forms = [...document.querySelectorAll(config.formSelector)];
-  forms.forEach(addListenersToForm);
-}
-enableValidation(validationConfig);
-function addListenersToForm(form) {
-  const inputs = form.querySelectorAll('.popup__input');
+  _validateLength = () => {
+    if (this._element.validity.tooShort || this._element.validity.tooLong) {
+      this._element.setCustomValidity(
+        'Укажите строку больше 2 и меньше 30 символов.',
+      );
+    }
+  }
 
-  inputs.forEach(addListenersToInput);
+  _validateRequired = () => {
+    if (this._element.validity.valueMissing) {
+      this._element.setCustomValidity('Вы пропустили это поле.');
+    }
+  }
 
-  form.addEventListener('submit', handleSubmit);
-  form.addEventListener('input', handleFormInput);
-
-  setSubmitButtonState(form);
-}
-
-function handleFormInput(event) {
-  const {
-    currentTarget: form,
-    target: {
-      validity: {valid},
-    },
-  } = event;
-
-  setSubmitButtonState(form);
-}
-
-function setSubmitButtonState(form) {
-  const button = form.querySelector('.popup__button');
-  button.disabled = !form.checkValidity();
-  button.classList.toggle('popup__button_invalid', !form.checkValidity());
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const {target: form} = event;
-
-}
-
-function addListenersToInput(input) {
-  input.addEventListener('input', handleFieldValidation);
-}
-
-function handleFieldValidation(event) {
-  const {target: element} = event;
-  element.setCustomValidity('');
-
-  const errorContainer = document.querySelector(`#${element.id}-error`);
-
-  validateLength(element);
-  validateRequired(element);
-  validateLink(element);
-
-  errorContainer.textContent = element.validationMessage;
-
-  element.classList.toggle(
-    'popup__input_state_invalid',
-    !element.validity.valid,
-  );
-}
-
-function validateLength(inputElement) {
-  if (inputElement.validity.tooShort || inputElement.validity.tooLong) {
-    inputElement.setCustomValidity(
-      'Укажите строку больше 2 и меньше 30 символов.',
-    );
+  _validateLink = () => {
+    if (this._element.type === 'url' && this._element.validity.patternMismatch) {
+      this._element.setCustomValidity('Введите адрес сайта');
+    }
   }
 }
 
-function validateRequired(inputElement) {
-  if (inputElement.validity.valueMissing) {
-    inputElement.setCustomValidity('Вы пропустили это поле.');
-  }
-}
-
-function validateLink(inputElement) {
-  if (inputElement.type === 'url' && inputElement.validity.patternMismatch) {
-    inputElement.setCustomValidity('Введите адрес сайта');
-  }
-}*/
